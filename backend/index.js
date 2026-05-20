912dd86e70f8fab4d94bdb6d6abe9c824f0b9b98
Shounak Pandit <68001165+shounak-create@users.noreply.github.com>
2026-05-21 04:45:01 +0530

    socket backend done

--- backend/index.js
+++ backend/index.js
@@ -35,8 +35,53 @@ app.use("/api/messages", messageRoutes);
 io.on("connection", (socket) => {
   console.log("User Connected:", socket.id);
 
-  socket.on("disconnect", () => {
+  // Setup user socket
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+
+    console.log("User Joined:", userData._id);
+
+    socket.emit("connected");
+  });
+
+  // Join chat room
+  socket.on("join chat", (room) => {
+    socket.join(room);
+
+    console.log("Joined Room:", room);
+  });
+
+  // Typing indicator
+  socket.on("typing", (room) => {
+    socket.in(room).emit("typing");
+  });
+
+  socket.on("stop typing", (room) => {
+    socket.in(room).emit("stop typing");
+  });
+
+  // Send message
+  socket.on("new message", (newMessage) => {
+    const chat = newMessage.chat;
+
+    if (!chat.users) return;
+
+    chat.users.forEach((user) => {
+
+      // Don't send to self
+      if (user._id == newMessage.sender._id) return;
+
+      // Send message to each user
+      socket.in(user._id).emit(
+        "message received",
+        newMessage
+      );
+    });
+  });
+
+  socket.off("setup", () => {
     console.log("User Disconnected");
+    socket.leave();
   });
 });
 
